8a98b7aebad28873ff0634df17f733bdb34b2bff
Eirik Hanssen <eirikhanssen79@gmail.com>
2015-08-13 08:02:56 +0200

    add content to parent information page

--- 06/foreldre.php
+++ 06/foreldre.php
@@ -1,15 +1,92 @@
-<!DOCTYPE html>
+<!--<!DOCTYPE html>-->
 <html lang="no">
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title>Til foreldre</title>
 	<link rel="stylesheet" href="../css/chapmenu.css" />
+	<style>
+		body {
+			line-height: 1.5em;
+		}
+		
+		img {
+			border: 1px solid black;
+			padding: 5px;
+			background: white;
+			margin: 1em auto;
+			display: block;
+		}
+		
+		figcaption {
+			text-align: center;
+			font-style: italic;
+		}
+	</style>
 </head>
 
 <body>
 	<h1>Til foreldre</h1>
+	<h2 id="om">Om nettsiden/prototypen</h2>
+	<p>Denne nettsiden/prototypen er en flerspråklig læringsressurs i naturfag. Nettsiden er ikke ferdig, men vi ønsker å prøve den ut slik at den ferdige versjonen kan bli best mulig. Vi vil gjerne ha kommentarer fra dere på hvordan ressursen fungerer.</p>
+	<p>Svar på denne <a href="http://nafo.empatix.no/evaluering-av-flerspraklig-naturfag">korte spørreundersøkelsen</a> for å si hva dere mener.</p>
+	<figure><img src="graphics/meitemark01.png" alt="meitemark" />
+		<figcaption>En meitemark</figcaption>
+	</figure>
+	<h2 id="bakgrunn">Bakgrunn</h2>
+	<p>Bakgrunnen for å lage en flerspråklig læringsressurs i naturfag er at vi ønsker at alle kan lære seg naturfag samtidig med å lære seg norsk. Ved å ha tekst og lyd på både somali, tigrinja, arabisk og norsk har man gode muligheter for dette.</p>
+	<h2 id="maal">Målet med dette kapitlet</h2>
+	<p>I naturfag skal elevene lære hvordan det er sammenheng mellom alt i naturen. Dette kapitlet handler om meitemarkens verden, og her kan elevene blant annet lære om hvordan det ser ut under bakken og hvordan meitemarken lager god jord.</p>
+	<figure><img src="graphics/meitemark03_hviler.png" />
+		<figcaption>En meitemark som hviler seg</figcaption>
+	</figure>
+	<h2 id="bruk">Hvordan kan du bruke denne nettsiden sammen med barnet ditt?</h2>
+	<p> Begynn gjerne med å se på bildet og snakk med barnet ditt på det språket du kan best om hva dere ser på bildet. Hva tror dere dette kan handle om, har dere sett noen av disse dyra før, eventuelt hvor, hvordan så de/den ut osv.</p>
+	<p>Gå så til den lange fortellingen og trykk på de språkene dere ønsker å se teksten/lytte til fortellingen på. Lytt til historien og følg gjerne med i teksten. Eller kanskje kan du lese historien for barnet ditt? Stopp gjerne underveis for å snakke om ord som er vanskelige eller ting som dere kommer på er naturlig å snakke om. Etter dere har hørt den lange fortellingen, så snakk med barnet om hva de tror vil skje videre. Hva skal de gjøre i mørket? Hva ville du ha gjort hvis du kunne gjøre deg selv så liten eller så stor du ville? Tenk om du var med i meitemarkens ganger. Hva ville du ha vært mest redd for? Hva ville vært mest morsomt?</p>
+	<p>Trykk så på den korte fortellingen. Denne teksten er ment som en tekst barnet kan lese selv. La barnet selv velge hvilket språk det vil lese på. Be barnet lese for deg eller les den korte teksten sammen.</p>
+	<p>Trykk så på oppgaver og gjør kvissen for å sjekke hva dere husker av det dere har lest/lyttet til. Spør barnet hvilke av de andre oppgavene det har lyst til å gjøre.</p>
+	<p>Hvis dere trykker på knappen filmer finner du lenker til filmer som handler om meitemarken.</p>
+	<p>Ordlisten er ennå ikke slik den skal være, men inneholder nå bare ett ord. Tanken er at elevene skal kunne trykke på elementer i bildet for å få opp forklaringer på hva ting er, eller trykke på ordliste for å få en forklaring på sentrale ord.</p>
+	<figure><img src="graphics/skolopender01.png" />
+		<figcaption>Skolopenderen er en av meitemarkens fiender</figcaption>
+	</figure>
+	<h2 id="mer_info">Ønsker du mer informasjon og råd om tospråklig utvikling?</h2>
+	<p>På nettsiden Tema morsmål finner du mye nyttig informasjon om morsmål og tospråklig opplæring. Denne siden kan det være lurt å følge med på jevnlig, siden det stadig legges ut nyheter. Knappene der det står foreldre og veiledning til foreldre er mest direkte rettet mot dere foreldre.</p>
+	<p>Barn i flerspråklige familier er et hefte som svarer på 10 spørsmål som foreldre ofte stiller om barnas språkutvikling. Et eksempel er: Kommer barnet til å lære norsk hvis vi snakker morsmålet hjemme? Heftet er oversatt til 18 språk. Du kan laste ned heftet <a href="http://www.morsmal.no/veiledning-til-foreldre/5135-informasjonsheftet-barn-i-flerspraklige-familier">«Barn i flerspråklige familier» på flere språk fra nettsiden Tema morsmål</a>.</p>
+	<p>Ønsker du tips om hvordan du kan støtte barnets tospråklige utvikling i snakking, lytting, lesing, skriving og matematikk? I heftene det er lenket til under, kan du finne tips og ideer til dette. Heftene finnes på 22 språk i tillegg til norsk.</p>
+	<h3 id="nedlasting">Hefter til nedlasting</h3>
+	<ul>
+		<li><a href="http://www.morsmal.no/veiledning-til-foreldre/5135-informasjonsheftet-barn-i-flerspraklige-familier">«Barn i flerspråklige familier»</a>
+		</li>
+		<li><a href="http://www.morsmal.no/veiledning-til-foreldre/1463-norske-kategorier/hjelp-barnet-ditt/5137-hjelp-barnet-ditt-med-a-lese-skrive-snakke-og-lytte">«Snakke, lytte, lese og skrive»</a>
+		</li>
+		<li><a href="http://www.morsmal.no/veiledning-til-foreldre/1463-norske-kategorier/hjelp-barnet-ditt/5167-a-arbeide-med-matematikk-sammen-med-barnet-ditt">«Matematikk»</a>
+		</li>
+	</ul>
+	<figure><img src="graphics/meitemarker_i_regnet_600x446.jpg" alt="meitemarker som ligger på asfalten i regnet" />
+		<figcaption>I regnet kommer meitemarken opp fra gangene i jorda.</figcaption>
+	</figure>
+	<h2>Last ned historien om meitemarken</h2>
+	<p>Hvis du vil skrive ut historien om barna i meitemarkens huler, kan du laste ned og skrive ut på flere språk i PDF-format:</p>
+	<ul>
+		<li>Norsk: <a href="nedlasting/meitemark_lager_god_jord_kort_tekst_norsk.pdf">Meitemark lager god jord (kortversjon)</a>
+		</li>
+		<li>Norsk: <a href="nedlasting/i_meitemarkens_verden_lang_tekst_norsk.pdf">I meitemarkens verden (lengre tekst)</a>
+		</li>
+		<li>Arabisk: <a href="nedlasting/meitemark_lager_god_jord_kort_tekst_arabisk.pdf">Meitemark lager god jord (kortversjon)</a>
+		</li>
+		<li>Arabisk: <a href="nedlasting/i_meitemarkens_verden_lang_tekst_arabisk.pdf">I meitemarkens verden (lengre tekst)</a>
+		</li>
+		<li>Somali: <a href="nedlasting/meitemark_lager_god_jord_kort_tekst_somali.pdf">Meitemark lager god jord (kortversjon)</a>
+		</li>
+		<li>Somali: <a href="nedlasting/i_meitemarkens_verden_lang_tekst_somali.pdf">I meitemarkens verden (lengre tekst)</a>
+		</li>
+		<li>Tigrinja: <a href="nedlasting/meitemark_lager_god_jord_kort_tekst_tigrinja.pdf">Meitemark lager god jord (kortversjon)</a>
+		</li>
+		<li>Tigrinja: <a href="nedlasting/i_meitemarkens_verden_lang_tekst_tigrinja.pdf">I meitemarkens verden (lengre tekst)</a>
+		</li>
+	</ul>
 	<?php include "phpinclude/chapmenu.php";?>
 	<script src="../js/classmod.js"></script>
 	<script src="../js/currentpage.js"></script>
