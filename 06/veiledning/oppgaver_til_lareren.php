49f7e0b90ce2a23adf73f8c08a3f801981a4e062
Eirik Hanssen <eirikhanssen79@gmail.com>
2015-08-13 12:18:08 +0200

    oppgaver til læreren wip

--- 06/veiledning/oppgaver_til_lareren.php
+++ 06/veiledning/oppgaver_til_lareren.php
@@ -19,7 +19,92 @@
 		<h1>Lærerveiledning</h1>
 		<section>
 			<h2 id="om">Oppgaver til læreren om meitemark</h2>
-			<p>TODO</p>
+			<p>Her er forslag til aktiviteter som strekker seg over dager og uker</p>
+			<ol>
+				<li><h3>Idemyldring</h3>
+				 <ul>
+					 <li>Hva vet elevene fra før om meitemark?</li>
+					 <li>Har de sett noen?</li>
+					 <li>Hvor?</li>
+					 <li>Var det tørt eller vått på bakken?</li>
+				</ul>
+				<p>Diskuter ut fra grubletegninger hentet fra <a href="http://www.naturfag.no">www.naturfag.no</a>:</p>
+				<ul>
+					<li><a href="http://www.naturfag.no/binfil/download2.php?tid=1293489">Meitemark</a></li>
+					<li><a href="http://www.naturfag.no/binfil/download2.php?tid=1327182">Kompostbinge</a></li>
+				</ul>
+				</li>
+				<li>
+					<h3>Film og bilder</h3>
+					<p>Vis bilder eller små filmer om meitemark (f.eks. fra youtube) og se i oppslagsverk og bøker om meitemark og/eller småkryp.</p>
+					<p>Bruk gjerne småkrypatlaset fra <a href="http://www.naturfag.no">www.naturfag.no</a></p>
+					<ul>
+						<li><a href="http://www.naturfag.no/utstyrsbeskrivelse/vis.html?tid=2112437">Småkryp i jord</a></li>
+						<li><a href="http://www.naturfag.no/binfil/download2.php?tid=2112430">Småkrypatlas</a></li>
+					</ul>
+					<p>Det er antakelig ikke alle barn som har sett meitemark, eller de har sett en uten å vite hva det var.</p>
+				</li>
+				<li>
+					<h3>TØL-skjema</h3>
+					<p>Elevene snakker sammen to og to: Hva vet de om meitemark?</p><p> De eller læreren oppsummerer på stort ark som henges på veggen.</p> <p>Arket kan oppdateres etter hvert som dere lærer mer. Da kan elevene følge utviklingen av sin egen læring.</p></li>
+				<li>
+					<h3>Paroppgaver</h3>
+				<p>Elevene snakker sammen to og to og spør hverandre:</p> <ul>
+					<li>Hva lurer du på om meitemarken?</li>
+					<li>Hva vil du undersøke?</li>
+				</ul> <p>Oppsummer skriftlig i fellesskap og heng det dere skriver på veggen.</p></li>
+				<li>
+					<h3>Ut og finn meitemark</h3>
+				<p>Læreren har forberedt hvordan mark skal anskaffes, enten ved at elevene går ut og finner, eller ved at læreren har skaffa jord og mark.</p> <p>Meitemark kan studeres i en plastboks med jord.</p> 
+				<ul>
+					<li>Små håndluper er smart å ha til alle elevene.</li>
+					<li>Fikk elevene vite noe nytt?</li>
+					<li>Så de børstene som marken går med?</li>
+					<li>Skriv opp og heng på veggen.</li>
+					<li>Kanskje det passer med en film nå?</li>
+				</ul>
+				<p>Se <a href="https://tv.nrk.no/serie/ut-i-naturen/dvna60000613/29-10-2013">Ut i naturen - Marksommer på NRK</a></p>
+				<img src="../graphics/flere_meitemark_agropub.jpg" alt="Flere meitemarker sett i forhold til en binders"/>
+				</li>
+				<li>
+					<h3>Terrarium</h3>
+					<p>Det gir mye større innsikt og er enda mer spennende med et terrarium. Elevene kan være med på å planlegge hva det skal være i det, skrive lister og finne ting.</p>
+					<ul>
+						<li>Hva trenger meitemarken for å overleve?</li>
+						<li>Hva finnes i leveområdet til meitemarken?</li>
+						<li>Det kan være viktig for elevene å finne ut hva meitemarkene liker å spise, - og hvordan kan de undersøke dette?</li>
+					</ul>
+					 
+					<p>Det er mange klasser som har gjort dette, erfaringer og oppskrifter finnes. Se noen av de her.</p>
+					<h4>Noen oppskrifter</h4>
+					<ul>
+						<li><a href="http://www.naturfag.no/uopplegg/vis.html?tid=2113259">Hvor blir det av? Om jord, løv og meitemarker</a></li>
+						<li><a href="http://www.naturfag.no/artikkel/vis.html?tid=2113420">Meitemark i klasserommet</a></li>
+						<li><a href="http://www.naturfag.no/forsok/vis.html?tid=2113486&within_tid=2113429">Meitemarkkompost</a></li>
+					</ul>
+				</li>
+				<li>
+				<h3>Oppsummering</h3>
+					<p>Etter en tid:</p>
+					<ul>
+						<li>Hva har elevene lært om leveområdet til meitemarken?</li>
+						<li>Hva spiser den, hva gjør den, hvorfor trenger den fuktighet, hvorfor liker den at det er mørkt … Oppsummer, skriv, heng på veggen. Sammenlikn med det som ble skrevet i starten.</li>
+						<li>Hva har elevene lært? Hva har elevene gjort, som har ført til at de veit mer? Skriv opp.</li></ul>
+				</li>
+				<li>
+					<h3>Nye ord og begreper</h3>
+					<p>Hvilke nye ord har elevene lært? Samtal med gruppa. Læreren bør ha en plan fra starten om hvilke ord som elevene skal få. Det kan være naturfagord som meitemark, jord, leveområde, kokong … Det kan være ord som forskere bruker, som observere, finne bevis. Finnes det ord som har en litt annen betydning i naturfag? Lag en liste med naturfagsord og hverdagsord.</p>
+					
+				</li>
+				<li>
+					<h3>Samarbeid</h3>
+					<p>Hvordan klarte elevene å samarbeide? Diskuter med elevene hva det vil si å samarbeide.</p>
+				</li>
+			</ol>
+			<h3>Tips — Skjemaer</h3>
+			<p>Se ulike skjemaer og skriv dem ut lenger ned på siden!</p>
+			<h3>Forskerføtter og leserøtter</h3>
+			<object id="forskning" type="image/svg+xml" data="forskning.svg" width="600" height="400" border="1"></object>
 			</section>
 		</section>
 	</main>
