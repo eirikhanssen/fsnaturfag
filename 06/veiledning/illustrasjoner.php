668e202a2709eff8b6c288e8224743916babec4e
Eirik Hanssen <eirikhanssen79@gmail.com>
2015-08-14 12:09:15 +0200

    add links to image folders and include licence information

--- 06/veiledning/illustrasjoner.php
+++ 06/veiledning/illustrasjoner.php
@@ -18,7 +18,41 @@
 	<main>
 		<h1>Lærerveiledning</h1>
 		<h2>Illustrasjoner</h2>
-		<p>Her kommer litt informasjon om rettigheter og bruk av illustrasjonene i prototypen og lenke til mappen med filene.</p>
+		<p>Illustrasjonene til læringsressursen finner du i disse mappene:</p>
+		<ul>
+			<li>
+				<a href="../graphics/">Illustrasjonene brukt i prototypen til nettressursen i web-størrelser.</a>
+			</li>
+			<li>
+				<a href="../graphics/fullsize/">Illustrasjoner til læringsressursen i full størrelse.</a>
+			</li>
+		</ul>
+		<p>Men før du tar de i bruk, bør du sette deg inn i vilkårene som er angitt under.</p>
+		<h3>Rettighetshaver</h3>
+		<p>Alle illustrasjonene, med unntak av noen få, er laget av Svetlana Voronkova for Norsk senter for flerkulturell opplæring (NAFO), og det er NAFO som står som rettighetshaver for alle illustrasjonene.</p>
+		<h3>Bruk av illustrasjonene</h3>
+		<p>Alle illustrasjonene i denne flerspråklige naturfagsressursen kan du benytte, men under betingelsene gitt i lisensen Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0).</p>
+		<p>Se lenker til lisensen <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/deed.no">CC BY-NC-SA 4.0 på norsk</a> og <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0 på engelsk</a>.</p>
+		<p>Under følger lisensteksten gitt på norsk.</p>
+		<h3>Creative Commons</h3>
+		<p>Creative Commons har flere forskjellige Open Access (fri tilgang) lisenser</p>
+		<p>For materiale lisensiert med CC BY-NC-SA 4.0 som gjelder for tekstene og illustrasjonene i denne læringsressursen, gjelder følgende betingelser:</p>
+		<h4>Du har lov til</h4>
+		<ul>
+			<li><strong>Dele</strong> — kopiere, distribuere og spre verket i hvilket som helst medium eller format</li>
+			<li><strong>Bearbeide</strong> — remixe, endre, og bygge videre på materialet</li>
+		</ul>
+		<p>Lisensgiver kan ikke kalle tilbake disse frihetene så lenge du respekterer disse lisensvilkårene.</p>
+		<h4>På følgende vilkår</h4>
+		<ul>
+			<li><strong>Navngivelse</strong> — Du må oppgi korrekt kreditering, oppgi en lenke til lisensen, og indikere om endringer er blitt gjort. Du kan kan gjøre dette på enhver rimelig måte, men uten at det kan forstås slik at lisensgiver bifaller deg eller din bruk av verket.</li>
+			<li><strong>IkkeKommersiell</strong> — Du kan ikke benytte materialet til kommersielle formål.</li>
+			<li><strong>DelPåSammeVilkår</strong> — Dersom du remixer, bearbeider eller bygger på materialet, må du distribuere dine bidrag under samme lisens som originalen.</li>
+		</ul>
+		<p><strong>Ingen ytterligere begrensninger</strong> — Du kan ikke gjøre bruk av juridiske betingelser eller teknologiske tiltak som lovmessig hindrer andre i å gjøre noe som lisensen tillater.</p>
+		<h4>Notiser</h4>
+		<p>Du trenger ikke å rette deg etter lisensen for de deler av materialet som er falt i det fri eller der bruken er tillatt av etter lånereglene i åndsverkloven eller annen gjeldende rett.</p>
+		<p>Ingen garantier er gitt. Lisensen gir deg ikke nødvendigvis alle de tillatelser som er nødvendig for din tiltenkte bruk. For eksempel kan andre rettigheter, som reklame-, personvern-, eller ideelle rettigheter, sette begrensninger på hvordan du kan bruke materialet.</p>
 	</main>
 	<?php $folder_level=1; include "../phpinclude/chapmenu.php";?>
 	<script src="../../js/classmod.js"></script>
